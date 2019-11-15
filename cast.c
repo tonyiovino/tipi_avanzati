6a4cbb3f895232de52bf8a30ce2bd9f253deced2
tonyiovino <49679221+tonyiovino@users.noreply.github.com>
2019-11-15 11:04:44 +0100

    Update cast.c

--- cast.c
+++ cast.c
@@ -7,14 +7,14 @@ int main()
 	int i_user;
 
 	printf("Inserica un numero con la virgola./n");
-	scanf("%d", d_user);
+	scanf("%lf", d_user);
 
 	double d_user = (int)d_user;
 	float f_user = (float)f_user;
 	int i_user = (int)i_user;
 
 	printf("Il numero intero è d%", &i_user);
-	printf("Il numero decimale è d%", &f_user);
+	printf("Il numero decimale è f%", &f_user);
 
 	return 0;
 }
