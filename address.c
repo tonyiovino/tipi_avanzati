8d80800597a3c1bdae854ae672cf839c52f8f5bd
tonyiovino <antonioiovino2004tony@gmail.com>
2019-11-17 15:18:29 +0100

    Address

---
+++ address.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	float number = 7.87;
+
+	float *adress_number;
+
+	address_number = &number;
+
+	printf("Questo è l'indirizzo del numero che ha inserito: %p\n", address_number);
+
+	return 0;	
+}
