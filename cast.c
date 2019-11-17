bb707c37c5d7434c1a62a1d2fe00f71aa8dd4d05
tonyiovino <antonioiovino2004tony@gmail.com>
2019-11-17 14:15:12 +0100

    Cast

--- cast.c
+++ cast.c
@@ -8,7 +8,7 @@ int main()
 	printf("Inserica un numero con la virgola: ");
 	scanf("%lf", &d_user);
 
-	d_user = (int)i_user;
+	i_user = (int)d_user;
 
 	printf("Il numero intero è %d.\n", i_user);
 	printf("Il numero decimale è %f.\n", d_user);
